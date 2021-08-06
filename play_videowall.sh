53ffc0a1a28b6be987f9ea7aba0075dbfe5d9cfb
Keiran King <me@keiranking.com>
2021-08-05 23:36:30 -0400

    Catch errors and offer help

--- play_videowall.sh
+++ play_videowall.sh
@@ -4,6 +4,20 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+function help_text {
+  echo "Options and arguments:"
+  echo "-c <color>     tint video files; <color> must be 6-digit hexadecimal"
+  echo "-f <filename>  set playlist filename; must be of type '.m3u'"
+  echo "-h             print this help message and exit"
+  echo "-r             include video files in subfolders (recursively)"
+  echo "-t <integer>   set duration of each video to <integer> seconds"
+}
+
+function usage_text {
+  echo "Usage: ./play_videowall.sh [-c <color>] [-f <filename>]"
+  echo "    [-h] [-r] [-t <integer>]"
+}
+
 # Set defaults
 playlist_name="$(basename "$PWD").m3u"
 clip_duration_in_seconds=10
@@ -12,15 +26,27 @@ color_tint="3b1e00"
 is_recursive=false
 
 # Process flags and override defaults
-optstring="c:f:rt:"
+optstring=":c:f:hrt:"
 while getopts ${optstring} arg; do
   case "${arg}" in
     c) color_tint=${OPTARG};;
     f) playlist_name=${OPTARG};;
+    h)
+      usage_text
+      echo
+      help_text
+      exit 0
+      ;;
     r) is_recursive=true;;
     t) clip_duration_in_seconds=${OPTARG};;
+    ?)
+      echo "Unknown option: -$OPTARG" 1>&2
+      usage_text
+      exit 1
+      ;;
   esac
 done
+shift $((OPTIND -1))
 
 echo "Generating playlist..."
 python3 build_playlist.py $clip_duration_in_seconds $playlist_duration_in_hours "$playlist_name" $is_recursive
